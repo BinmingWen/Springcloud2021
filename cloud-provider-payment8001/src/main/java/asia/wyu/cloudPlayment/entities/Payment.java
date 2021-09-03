2771eb67d7d32b41d99cc75c1e99248ccaa75898
wen <18027761141@163.com>
2021-09-03 18:11:40 +0800

    add entities class of projects

---
+++ cloud-provider-payment8001/src/main/java/asia/wyu/cloudPlayment/entities/Payment.java
@@ -0,0 +1,23 @@
+package asia.wyu.cloudPlayment.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @class: asia.wyu.cloudPlayment.entities.Payment
+ * @description:
+ * @author: 温明彬
+ * @company: 广州博瑞信息技术股份有限公司
+ * @create: 2021/9/3 18:09
+ */
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Payment implements Serializable {
+    private Long id;
+    private String serial;
+}
