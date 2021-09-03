e7e2494a25b3156c9ae5ca80fb65b882498374f3
wen <18027761141@163.com>
2021-09-03 14:06:37 +0800

    Initial commit

---
+++ cloud-provider-payment8001/src/main/java/asia/wyu/cloudPlayment/CloudProviderPayment8001.java
@@ -0,0 +1,19 @@
+package asia.wyu.cloudPlayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @class: asia.wyu.cloudPlayment.CloudProviderPayment8001
+ * @description:
+ * @author: 温明彬
+ * @company: 广州博瑞信息技术股份有限公司
+ * @create: 2021/9/3 9:37
+ */
+
+@SpringBootApplication
+public class CloudProviderPayment8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProviderPayment8001.class,args);
+    }
+}
