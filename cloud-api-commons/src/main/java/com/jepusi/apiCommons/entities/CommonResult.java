a719d9833806eda112fc4252210116629db73eba
Tom <18027761141@163.com>
2021-10-31 22:52:47 +0800

    add consumer module

---
+++ cloud-api-commons/src/main/java/com/jepusi/apiCommons/entities/CommonResult.java
@@ -0,0 +1,26 @@
+package com.jepusi.apiCommons.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @class: asia.wyu.cloudPlayment.entities.CommonResult
+ * @description:
+ * @author: 温明彬
+ * @company: 广州博瑞信息技术股份有限公司
+ * @create: 2021/10/29 17:49
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonResult<T> {
+    private Integer code;
+    private String message;
+    private T data;
+
+    public CommonResult(Integer code,String message){
+        this(code,message,null);
+    }
+}
